96ac3b96a0638229dbfe55f902382448697d826b
n-r-b <nblanchette@companieshouse.gov.uk>
2020-08-14 10:07:27 +0100

    chore: update .editorconfig indent size

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,5 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 
 export class HomeController {
 
-    public sayHello = (req: Request, res: Response, next: NextFunction) => res.render('index');
+  public sayHello = (req: Request, res: Response, next: NextFunction) => res.render('index');
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
 app.listen(app.get('port'), () => {
-    console.log(`Open browser at http://localhost:${app.get('port')}`);
-    console.log('Press CTRL-C to stop\n');
+  console.log(`Open browser at http://localhost:${app.get('port')}`);
+  console.log('Press CTRL-C to stop\n');
 
 });
