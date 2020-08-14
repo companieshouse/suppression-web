5cb266c969f69b0ff8374dd1e529efbc9e955a46
n-r-b <nblanchette@companieshouse.gov.uk>
2020-08-14 09:15:25 +0100

    chore: remove auto formatting

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ const sass = require("gulp-sass");
 sass.compiler = require('node-sass');
 
 // copies the fonts and images from the govuk-frontend package to the dist directory
-gulp.task("govuk-frontend-copy", function () {
-    return gulp.src([
-        "./node_modules/govuk-frontend/govuk/assets/**/*"
-    ]).pipe(gulp.dest("./dist/static"));
+gulp.task("govuk-frontend-copy", function() {
+  return gulp.src([
+    "./node_modules/govuk-frontend/govuk/assets/**/*"
+  ]).pipe(gulp.dest("./dist/static"));
 });
 
 // compiles the sass down to css
-gulp.task('sass', function () {
-    return gulp.src('./src/static/sass/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/static'));
+gulp.task('sass', function() {
+  return gulp.src('./src/static/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/static'));
 });
 
 // executes all static asset tasks in parallel
 exports.static = gulp.parallel(
-    gulp.task("sass"),
-    gulp.task("govuk-frontend-copy")
+  gulp.task("sass"),
+  gulp.task("govuk-frontend-copy")
 );
