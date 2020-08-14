070e144f981ecc9914210d48359b7233a32bb957
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-08-14 14:43:25 +0100

    Remove redundant attribute in Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-    roots: [
-        "<rootDir>"
-    ],
     testPathIgnorePatterns: [
         "/node_modules/",
         "/dist/"
     ],
     transform: {
         "^.+\\.(ts|tsx)$": "ts-jest"
-    },
+    }
 }
