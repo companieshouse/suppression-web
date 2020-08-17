d313dcead6a5f8d337b2ec34ec1c7093163d3e59
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-08-17 12:43:48 +0100

    Rename HomeController, add tests

--- src/controllers/HomeController.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export class HomeController {
-
-  public sayHello = (req: Request, res: Response, next: NextFunction) => res.render('index');
-}
