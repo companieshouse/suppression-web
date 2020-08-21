9babf5da24973fbf2a3af78ae407bc50841337ae
n-r-b <nblanchette@companieshouse.gov.uk>
2020-08-21 13:13:45 +0100

    refact: CR changes

--- src/modules/config-handler/ConfigValidation.schema.ts
+++ src/modules/config-handler/ConfigValidation.schema.ts
@@ -1,10 +1,8 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
-  PORT: Joi.number()
-    .default(3000),
-  CDN_HOST: Joi.string()
-    .required()
+  PORT: Joi.number().default(3000),
+  CDN_HOST: Joi.string().required()
 }).options({
   allowUnknown: true
 });
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 import { getConfigValue } from './modules/config-handler/ConfigHandler';
 
 app.listen(app.get('port'), () => {
-  console.log(`Open browser at http://localhost:${app.get('port')} in ${getConfigValue('NODE_ENV')} mode`);
+  console.log(`Open browser at http://localhost:${getConfigValue('PORT')} in ${getConfigValue('NODE_ENV')} mode`);
   console.log('Press CTRL-C to stop\n');
 
 });
