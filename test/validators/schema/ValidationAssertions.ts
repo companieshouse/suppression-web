fd5608e8075ce5dd82f1e4257543d9f90442dd34
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-08-26 11:03:30 +0100

    Move ValidationAssertions test helper

--- test/validators/schema/ValidationAssertions.ts
+++ test/validators/schema/ValidationAssertions.ts
@@ -1,5 +1,5 @@
-import { ValidationError } from '../src/validators/utils/ValidationError';
-import { ValidationResult } from '../src/validators/utils/ValidationResult';
+import { ValidationError } from '../../../src/validators/utils/ValidationError';
+import { ValidationResult } from '../../../src/validators/utils/ValidationResult';
 
 export const assertValidationErrors = (result: ValidationResult, expectedErrors: ValidationError[]): void => {
   expect(result.errors).toHaveLength(expectedErrors.length);
