115febacfad9ab034d9453838de70ff7a909dca7
akaJSaunders <me@jsaunders.dev>
2020-08-27 11:09:03 +0100

    integrate express session

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import session from 'express-session';
 import * as nunjucks from 'nunjucks';
 import * as path from 'path';
+import IORedis from 'ioredis';
 
 import {
   getConfigValue,
@@ -11,6 +13,19 @@ import { configValidationSchema } from './modules/config-handler/ConfigValidatio
 import { routes } from './routes/routes';
 
 const app = express();
+
+const RedisStore = require('connect-redis')(session);
+
+app.use(session({
+  secret: getConfigValue('COOKIE_SECRET') as string,
+  name: getConfigValue('COOKIE_NAME'),
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, domain: getConfigValue('COOKIE_DOMAIN')},
+  store: new RedisStore({ client: new IORedis(getConfigValue('CACHE_SERVER')) })
+}));
+
+
 
 loadEnvironmentVariables({validationSchema: configValidationSchema});
 
--- src/controllers/StartPageController.ts
+++ src/controllers/StartPageController.ts
@@ -2,5 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 
 export class StartPageController {
 
-  public renderView = (req: Request, res: Response, next: NextFunction) => res.render('start-page');
+  public renderView = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.session);
+    res.render('start-page');
+  }
+  public start = (req: Request, res: Response) => {
+    console.log('POST REQUEST');
+    req.session!.data = {test: "testing the session"};
+    res.render('start-page');
+  };
 }
