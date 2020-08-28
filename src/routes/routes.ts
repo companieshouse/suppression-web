96fb3906ff9b8541f75ffe993102b04f63e36168
n-r-b <nblanchette@companieshouse.gov.uk>
2020-08-28 15:55:04 +0100

    feat: update routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import { ApplicantDetailsController } from '../controllers/ApplicantDetailsController';
 import { DocumentDetailsController } from '../controllers/DocumentDetailsController';
-import { ApplicantDetailsController } from '../controllers/ApplicantDetailsController'
 import { StartPageController } from '../controllers/StartPageController';
-import { DOCUMENT_DETAILS_PAGE_URI, ROOT_URI } from './paths';
-import { APPLICANT_DETAILS_PAGE_URI, ROOT_URI } from './paths';
+import { APPLICANT_DETAILS_PAGE_URI, DOCUMENT_DETAILS_PAGE_URI, ROOT_URI } from './paths';
 
 
 export const routes = Router();
@@ -12,14 +11,15 @@ export const routes = Router();
  * Controllers (route handlers).
  */
 const startPageController = new StartPageController();
-const documentDetailsController = new DocumentDetailsController();
 const applicantDetailsController = new ApplicantDetailsController();
+const documentDetailsController = new DocumentDetailsController();
 
 /**
  * Route definitions
  */
 routes.get(ROOT_URI, startPageController.renderView);
 routes.post(ROOT_URI, startPageController.start);
+
 routes.get(APPLICANT_DETAILS_PAGE_URI, applicantDetailsController.renderView);
 routes.post(APPLICANT_DETAILS_PAGE_URI, applicantDetailsController.processForm);
 
