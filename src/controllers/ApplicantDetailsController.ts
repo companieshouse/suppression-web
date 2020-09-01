5d79e49351458fef1dda54833f480c986ad337a2
Juliano Saunders <me@jsaunders.dev>
2020-09-01 14:01:44 +0100

    fix: back button bug with errors bug

--- src/controllers/ApplicantDetailsController.ts
+++ src/controllers/ApplicantDetailsController.ts
@@ -26,7 +26,8 @@ export class ApplicantDetailsController {
       res.status(StatusCodes.UNPROCESSABLE_ENTITY);
       return res.render(template, {
         ...req.body,
-        validationResult
+        validationResult,
+        backNavigation: previousURL
       });
     } else {
       const session = SessionService.getSuppressionSession(req);
