7efd2626835c41e27153a053639027bdf536927b
n-r-b <nblanchette@companieshouse.gov.uk>
2020-09-02 09:19:31 +0100

    feat: session persistence for document details

--- src/controllers/DocumentDetailsController.ts
+++ src/controllers/DocumentDetailsController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import moment from 'moment';
 
-import { SuppressionData } from '../models/SuppressionDataModel';
+import { DocumentDetails, SuppressionData } from '../models/SuppressionDataModel';
 import { DOCUMENT_DETAILS_PAGE_URI } from '../routes/paths';
 import SessionService from '../services/SessionService';
 import { DocumentDetailsValidator } from '../validators/DocumentDetailsValidator';
@@ -12,8 +14,15 @@ export class DocumentDetailsController {
   constructor(private dateValidator: DocumentDetailsValidator = new DocumentDetailsValidator()) {}
 
   public renderView = (req: Request, res: Response, next: NextFunction) => {
-    const suppression = SessionService.getSuppressionSession(req);
-    res.render(template, suppression);
+
+    const suppressionData = SessionService.getSuppressionSession(req);
+
+    let documentDetails = {};
+    if (suppressionData) {
+      documentDetails = this.getDocumentDetails(suppressionData);
+    }
+
+    res.render(template, {...documentDetails});
   }
 
   public processForm = async (req: Request, res: Response, next: NextFunction) => {
@@ -21,21 +30,35 @@ export class DocumentDetailsController {
     const validationResult = await this.dateValidator.validate(req);
 
     if (validationResult.errors.length > 0) {
-      res.status(422);
+      res.status(StatusCodes.UNPROCESSABLE_ENTITY);
       return res.render(template, {
         ...req.body,
         validationResult
       });
     }
 
-    const suppression: SuppressionData = SessionService.getSuppressionSession(req)!
+    const suppression: SuppressionData | undefined = SessionService.getSuppressionSession(req);
 
     const updatedSession: SuppressionData = {
       ...suppression,
-      documentDetails: req.body
+      documentDetails: {
+        ...req.body,
+        date: moment(req.body.date).format('YYYY-MM-DD')
+      }
     }
 
     SessionService.setSuppressionSession(req, updatedSession)
     res.redirect(DOCUMENT_DETAILS_PAGE_URI);
+  }
+
+  private getDocumentDetails(suppression: SuppressionData): any {
+
+    const documentDetails: DocumentDetails | undefined = suppression.documentDetails;
+    if (!documentDetails) {
+      return {};
+    }
+    const [year, month, day] = documentDetails.date.split('-', 3);
+
+    return {...documentDetails, day, month, year};
   }
 }
