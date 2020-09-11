7b6a335cb730a63a44bc7974d9fa959d9f501383
n-r-b <nblanchette@companieshouse.gov.uk>
2020-09-11 10:46:40 +0100

    feat: exclude landing page from authentication

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { authMiddleware, AuthOptions } from 'web-security-node';
 import { getConfigValue } from '../modules/config-handler/ConfigHandler';
-import { APPLICANT_DETAILS_PAGE_URI } from '../routes/paths';
+import { APPLICANT_DETAILS_PAGE_URI, ROOT_URI } from '../routes/paths';
 import { newUriFactory } from '../utils/UriFactory';
 
 
 export function AuthMiddleware(): RequestHandler {
 
   return (req: Request, res: Response, next: NextFunction): any => {
+
+    if (req.originalUrl === ROOT_URI) {
+      return next();
+    }
 
     const authOptions: AuthOptions = {
       returnUrl: newUriFactory(req).createAbsoluteUri(APPLICANT_DETAILS_PAGE_URI),
