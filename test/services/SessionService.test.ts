f8de2bbaada1361baa7514e97063fa6fe619be79
n-r-b <nblanchette@companieshouse.gov.uk>
2020-09-11 08:51:06 +0100

    feat: update session service tests

--- test/services/SessionService.test.ts
+++ test/services/SessionService.test.ts
@@ -1,66 +1,75 @@
-import { SuppressionData } from '../../src/models/SuppressionDataModel';
+import { Session } from 'ch-node-session-handler';
+import { Request } from 'express';
+import { SuppressionData, SUPPRESSION_DATA_KEY } from '../../src/models/SuppressionDataModel';
 import SessionService from '../../src/services/SessionService';
 
-const getSuppressionData: SuppressionData = {
-    applicantDetails: {fullName: 'test-name', emailAddress: 'test-email'},
-    addressToRemove: {
-      line1: '1 Test Street',
-      line2: '',
-      town: 'Test Town',
-      county: 'Test Midlands',
-      postcode: 'TE10 6ST'
-    },
-    documentDetails: {
-      companyName: 'company-name-test',
-      companyNumber: 'NI000000',
-      description: 'This is a document',
-      date: '2020-01-01'
-    }
+const mockSuppressionData: SuppressionData = {
+  applicantDetails: {
+    fullName: 'test-name',
+    emailAddress: 'test-email'
+  },
+  addressToRemove: {
+    line1: '1 Test Street',
+    line2: '',
+    town: 'Test Town',
+    county: 'Test Midlands',
+    postcode: 'TE10 6ST'
+  },
+  documentDetails: {
+    companyName: 'company-name-test',
+    companyNumber: 'NI000000',
+    description: 'This is a document',
+    date: '2020-01-01'
+  }
 };
+
+const mockRequestData = {
+  session: {
+    get: jest.fn(),
+    data: jest.fn(),
+    getExtraData: jest.fn(),
+    setExtraData: jest.fn(),
+    deleteExtraData: jest.fn(),
+    verify: jest.fn()
+  } as unknown as Session
+} as Request;
 
 describe('SessionService', () => {
 
   it('should retrieve suppression data from the session', () => {
 
-    const suppressionData: SuppressionData = getSuppressionData;
+    const suppressionData: SuppressionData = mockSuppressionData;
+    const mockRequest: Request = mockRequestData;
 
-    const mockRequest: any = {
-      session: { extra_data: { suppression:  suppressionData  } }
-    };
+    const mockGetExtraData = jest.fn().mockReturnValue(suppressionData);
+    mockRequest.session!.getExtraData = mockGetExtraData;
 
-
-    mockRequest.session!.getExtraData = jest.fn().mockReturnValue(suppressionData);
-
-    expect(SessionService.getSuppressionSession(mockRequest)).toEqual(suppressionData)
-
+    expect(SessionService.getSuppressionSession(mockRequest)).toEqual(suppressionData);
+    expect(mockGetExtraData).toHaveBeenCalledWith(SUPPRESSION_DATA_KEY);
   });
 
   it('should return undefined when no suppression data exists in the session', () => {
 
-    const mockRequest: any = {
-      session: { extra_data: {}  }
-    };
+    const mockRequest: Request = mockRequestData;
 
-    mockRequest.session!.getExtraData = jest.fn().mockReturnValue(undefined);
+    const mockGetExtraData: jest.Mock = jest.fn().mockReturnValue(undefined);
+    mockRequest.session!.getExtraData = mockGetExtraData;
 
     expect(SessionService.getSuppressionSession(mockRequest)).toBeUndefined();
-
+    expect(mockGetExtraData).toHaveBeenCalledWith(SUPPRESSION_DATA_KEY);
   });
 
   it('should set the suppression data in the session', () => {
 
-    const suppressionData: SuppressionData = getSuppressionData;
+    const suppressionData: SuppressionData = mockSuppressionData;
+    const mockRequest: Request = mockRequestData;
 
-    const mockRequest: any = {
-      session: { extra_data: {} }
-    };
-
-    const setExtraDataMock: jest.Mock = jest.fn();
-    mockRequest.session!.setExtraData = setExtraDataMock;
+    const mockSetExtraData: jest.Mock = jest.fn();
+    mockRequest.session!.setExtraData = mockSetExtraData;
 
     SessionService.setSuppressionSession(mockRequest, suppressionData);
 
-    expect(setExtraDataMock).toHaveBeenCalledWith('suppression', suppressionData);
+    expect(mockSetExtraData).toHaveBeenCalledWith(SUPPRESSION_DATA_KEY, suppressionData);
   })
 
 });
