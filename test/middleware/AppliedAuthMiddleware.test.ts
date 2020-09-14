4a2c4813cff3a9ca13d0ff8a2d21f8e98afbb590
n-r-b <nblanchette@companieshouse.gov.uk>
2020-09-14 12:12:16 +0100

    refact: organise auth middleware tests

--- test/middleware/AppliedAuthMiddleware.test.ts
+++ test/middleware/AppliedAuthMiddleware.test.ts
@@ -16,57 +16,64 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-describe('All pages after the landing page:', () => {
+describe('Applied auth middleware', () => {
 
-  const webSecurityModule = require('web-security-node');
+  describe('All pages after the landing page', () => {
 
-  const pageList = [
-    {name: 'Applicant Details', uri: APPLICANT_DETAILS_PAGE_URI},
-    {name: 'Address To Remove', uri: ADDRESS_TO_REMOVE_PAGE_URI},
-    {name: 'Document Details', uri: DOCUMENT_DETAILS_PAGE_URI},
-    {name: 'Payment Review', uri: PAYMENT_REVIEW_PAGE_URI},
-  ];
+    const webSecurityModule = require('web-security-node');
 
-  for (const page of pageList) {
-    it(`The ${page.name} page should redirect unauthenticated user to sign in page`, async () => {
+    const pageList = [
+      {name: 'Applicant Details', uri: APPLICANT_DETAILS_PAGE_URI},
+      {name: 'Address To Remove', uri: ADDRESS_TO_REMOVE_PAGE_URI},
+      {name: 'Document Details', uri: DOCUMENT_DETAILS_PAGE_URI},
+      {name: 'Payment Review', uri: PAYMENT_REVIEW_PAGE_URI},
+    ];
+
+    for (const page of pageList) {
+      it(`The ${page.name} page should redirect unauthenticated user to sign in page`, async () => {
+
+        const app = createApp(true);
+
+        jest.spyOn(webSecurityModule, 'authMiddleware')
+          .mockReturnValue((req, res, next) => {
+            return res.redirect('/fakeurl');
+          });
+
+        await request(app).get(page.uri)
+          .expect(response => {
+            expect(response.status).toEqual(StatusCodes.MOVED_TEMPORARILY);
+            expect(response.text).toContain('/fakeurl');
+          });
+      });
+
+      it(`The ${page.name} page should not redirect authenticated user to sign in page`, async () => {
+
+        const app = createApp(true);
+
+        jest.spyOn(webSecurityModule, 'authMiddleware')
+          .mockReturnValue((req, res, next) => {
+            return next();
+          });
+
+        await request(app).get(page.uri)
+          .expect(response => {
+            expect(response.status).toEqual(StatusCodes.OK);
+          });
+      });
+    }
+  });
+
+  describe('Landing page', () => {
+
+    it(`should not redirect unauthenticated user to sign in page `, async () => {
 
       const app = createApp(true);
 
-      jest.spyOn(webSecurityModule, 'authMiddleware')
-        .mockReturnValue((req, res, next) => {
-          return res.redirect('/fakeurl');
-        });
-
-      await request(app).get(page.uri)
-        .expect(response => {
-          expect(response.status).toEqual(StatusCodes.MOVED_TEMPORARILY);
-          expect(response.text).toContain('/fakeurl');
-        });
-    });
-
-    it(`The ${page.name} page should not redirect authenticated user to sign in page`, async () => {
-
-      const app = createApp(true);
-
-      jest.spyOn(webSecurityModule, 'authMiddleware')
-        .mockReturnValue((req, res, next) => {
-          return next();
-        });
-
-      await request(app).get(page.uri)
+      await request(app).get(ROOT_URI)
         .expect(response => {
           expect(response.status).toEqual(StatusCodes.OK);
         });
     });
-  }
-
-  it(`The landing page should not redirect unauthenticated user to sign in page `, async () => {
-
-    const app = createApp(true);
-
-    await request(app).get(ROOT_URI)
-      .expect(response => {
-        expect(response.status).toEqual(StatusCodes.OK);
-      });
   });
-})
+
+});
