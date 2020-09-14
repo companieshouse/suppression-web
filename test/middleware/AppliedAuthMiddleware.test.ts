82d3bd4e72462e8013375a3a64b8114eaeacd44c
n-r-b <nblanchette@companieshouse.gov.uk>
2020-09-14 15:48:32 +0100

    feat: rename tests for readability

--- test/middleware/AppliedAuthMiddleware.test.ts
+++ test/middleware/AppliedAuthMiddleware.test.ts
@@ -18,19 +18,32 @@ beforeEach(() => {
 
 describe('Applied auth middleware', () => {
 
-  describe('All pages after the landing page', () => {
+  const webSecurityModule = require('web-security-node');
 
-    const webSecurityModule = require('web-security-node');
+  const pageList = [
+    {name: 'Applicant Details', uri: APPLICANT_DETAILS_PAGE_URI},
+    {name: 'Address To Remove', uri: ADDRESS_TO_REMOVE_PAGE_URI},
+    {name: 'Document Details', uri: DOCUMENT_DETAILS_PAGE_URI},
+    {name: 'Payment Review', uri: PAYMENT_REVIEW_PAGE_URI},
+  ];
 
-    const pageList = [
-      {name: 'Applicant Details', uri: APPLICANT_DETAILS_PAGE_URI},
-      {name: 'Address To Remove', uri: ADDRESS_TO_REMOVE_PAGE_URI},
-      {name: 'Document Details', uri: DOCUMENT_DETAILS_PAGE_URI},
-      {name: 'Payment Review', uri: PAYMENT_REVIEW_PAGE_URI},
-    ];
+  describe('Unauthenticated user: landing page', () => {
+
+    it('should not redirect user to sign in page', async () => {
+
+      const app = createApp(true);
+
+      await request(app).get(ROOT_URI)
+        .expect(response => {
+          expect(response.status).toEqual(StatusCodes.OK);
+        });
+    });
+  });
+
+  describe('Unauthenticated user: all pages after the landing page', () => {
 
     for (const page of pageList) {
-      it(`The ${page.name} page should redirect unauthenticated user to sign in page`, async () => {
+      it(`should redirect from ${page.name} to sign in page`, async () => {
 
         const app = createApp(true);
 
@@ -45,8 +58,15 @@ describe('Applied auth middleware', () => {
             expect(response.text).toContain('/fakeurl');
           });
       });
+    }
+  });
 
-      it(`The ${page.name} page should not redirect authenticated user to sign in page`, async () => {
+  describe('Authenticated user: all pages', () => {
+
+    pageList.push({name: 'Landing Page', uri: ROOT_URI});
+
+    for (const page of pageList) {
+      it(`should not redirect from ${page.name} to sign in page`, async () => {
 
         const app = createApp(true);
 
@@ -61,19 +81,6 @@ describe('Applied auth middleware', () => {
           });
       });
     }
-  });
-
-  describe('Landing page', () => {
-
-    it('should not redirect unauthenticated user to sign in page', async () => {
-
-      const app = createApp(true);
-
-      await request(app).get(ROOT_URI)
-        .expect(response => {
-          expect(response.status).toEqual(StatusCodes.OK);
-        });
-    });
   });
 
 });
