5e76572a6a58650d978b7d539aaccbf1851ac8f6
akaJSaunders <me@jsaunders.dev>
2020-09-21 12:16:15 +0100

    add controller tests

--- test/controllers/ApplicantDetailsController.test.ts
+++ test/controllers/ApplicantDetailsController.test.ts
@@ -37,23 +37,28 @@ describe('ApplicantDetailsController', () => {
   describe('on POST', () => {
 
     const fullNameErrorMessage = 'Full name is required';
+    const hasPreviousNameMissingMessage = 'Select yes if the applicant has used a different name for business purposes in the last 20 years';
+    const previousNameMissingMessage = 'Enter previous full names, used for business purposes';
     const emailMissingErrorMessage = 'Email address is required';
     const emailInvalidErrorMessage = 'Enter an email address in the correct format, like name@example.com';
 
-    it('should show two validation errors if no information is entered', async () => {
+    it('should show three validation errors if no information is entered', async () => {
 
       await request(app).post(APPLICANT_DETAILS_PAGE_URI).expect(response => {
         expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
         expectToHaveTitle(response.text, pageTitle);
         expectToHaveBackButton(response.text, ROOT_URI);
-        expectToHaveErrorSummaryContaining(response.text, [fullNameErrorMessage, emailMissingErrorMessage]);
-        expectToHaveErrorMessages(response.text, [fullNameErrorMessage, emailMissingErrorMessage]);
+        expectToHaveErrorSummaryContaining(response.text, [fullNameErrorMessage, hasPreviousNameMissingMessage, emailMissingErrorMessage]);
+        expectToHaveErrorMessages(response.text, [fullNameErrorMessage, hasPreviousNameMissingMessage, emailMissingErrorMessage]);
       });
     });
 
     it('should show a validation error if no name is entered', async () => {
 
-      await request(app).post(APPLICANT_DETAILS_PAGE_URI).send({ emailAddress: 'test@example.com' }).expect(response => {
+      await request(app).post(APPLICANT_DETAILS_PAGE_URI)
+        .send({ hasPreviousName: 'yes', previousName: 'test_name', emailAddress: 'test@example.com' })
+        .expect(response => {
+
         expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
         expectToHaveTitle(response.text, pageTitle);
         expectToHaveBackButton(response.text, ROOT_URI);
@@ -62,9 +67,40 @@ describe('ApplicantDetailsController', () => {
       });
     });
 
+    it('should show a validation error if no hasPreviousName option entered', async () => {
+
+      await request(app).post(APPLICANT_DETAILS_PAGE_URI)
+        .send({ fullName: 'John Doe', emailAddress: 'test@example.com' })
+        .expect(response => {
+
+          expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
+          expectToHaveTitle(response.text, pageTitle);
+          expectToHaveBackButton(response.text, ROOT_URI);
+          expectToHaveErrorSummaryContaining(response.text, [hasPreviousNameMissingMessage]);
+          expectToHaveErrorMessages(response.text, [hasPreviousNameMissingMessage]);
+        });
+    });
+
+    it('should show a validation error if hasPreviousName option is yes, but no name entered', async () => {
+
+      await request(app).post(APPLICANT_DETAILS_PAGE_URI)
+        .send({ fullName: 'John Doe', hasPreviousName: 'yes', emailAddress: 'test@example.com' })
+        .expect(response => {
+
+          expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
+          expectToHaveTitle(response.text, pageTitle);
+          expectToHaveBackButton(response.text, ROOT_URI);
+          expectToHaveErrorSummaryContaining(response.text, [previousNameMissingMessage]);
+          expectToHaveErrorMessages(response.text, [previousNameMissingMessage]);
+        });
+    });
+
     it('should show a validation error if no email address is entered', async () => {
 
-      await request(app).post(APPLICANT_DETAILS_PAGE_URI).send({ fullName: 'John Doe' }).expect(response => {
+      await request(app).post(APPLICANT_DETAILS_PAGE_URI)
+        .send({ fullName: 'John Doe', hasPreviousName: 'yes', previousName: 'test_name'})
+        .expect(response => {
+
         expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
         expectToHaveTitle(response.text, pageTitle);
         expectToHaveBackButton(response.text, ROOT_URI);
@@ -77,7 +113,10 @@ describe('ApplicantDetailsController', () => {
 
       await request(app).post(APPLICANT_DETAILS_PAGE_URI).send({
         fullName: 'John Doe',
+        hasPreviousName: 'yes',
+        previousName: 'test_name',
         emailAddress: 'test.com'
+
       }).expect(response => {
         expect(response.status).toEqual(StatusCodes.UNPROCESSABLE_ENTITY);
         expectToHaveTitle(response.text, pageTitle);
@@ -87,10 +126,24 @@ describe('ApplicantDetailsController', () => {
       });
     });
 
-    it('should redirect to the next page if the information provided by the user is valid', async () => {
+    it('should redirect to the next page if the information provided by the user is valid (yes to previousNames)', async () => {
 
       await request(app).post(APPLICANT_DETAILS_PAGE_URI).send({
         fullName: 'John Doe',
+        hasPreviousName: 'yes',
+        previousName: 'test_name',
+        emailAddress: 'test@example.com'
+      }).expect(response => {
+        expect(response.status).toEqual(StatusCodes.MOVED_TEMPORARILY);
+        expect(response.header.location).toContain(ADDRESS_TO_REMOVE_PAGE_URI);
+      });
+    });
+
+    it('should redirect to the next page if the information provided by the user is valid (no to previousName)', async () => {
+
+      await request(app).post(APPLICANT_DETAILS_PAGE_URI).send({
+        fullName: 'John Doe',
+        hasPreviousName: 'no',
         emailAddress: 'test@example.com'
       }).expect(response => {
         expect(response.status).toEqual(StatusCodes.MOVED_TEMPORARILY);
