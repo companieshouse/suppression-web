aed247fe2e577c11bd673684181ab09c092700c7
Rhys Barrett <rbarrett@companieshouse.gov.uk>
2020-09-25 09:47:38 +0100

    Amend function definition

--- src/controllers/HealthcheckController.ts
+++ src/controllers/HealthcheckController.ts
@@ -1,9 +1,9 @@
-import { Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 export class HealthcheckController {
 
-  public healthcheck = (_: any, res: Response, __: any) => {
+  public healthcheck = (req: Request, res: Response, next: NextFunction) => {
     res.status(StatusCodes.OK).send({ 'status': 'OK'});
   };
 }
