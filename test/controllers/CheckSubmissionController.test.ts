849696a893b1fb9b4aaee0f3544a44a24351a6a8
akaJSaunders <me@jsaunders.dev>
2020-10-01 16:16:59 +0100

    add contact details to check your answers page

--- test/controllers/CheckSubmissionController.test.ts
+++ test/controllers/CheckSubmissionController.test.ts
@@ -55,6 +55,9 @@ describe('CheckSubmissionController', () => {
             'What address do you want to replace your home address with\\?',
             '1 Main Street<br>Cardiff<br>Cardiff<br>CF14 3UZ<br>United Kingdom');
           expectToHaveTableRow(response.text, 'Email address', 'test@example.com');
+          expectToHaveTableRow(response.text,
+            'Contact address',
+            '1st Avenue<br>New York<br>New York<br>NY<br>USA');
           expectToHaveButton(response.text, 'Accept and submit');
         });
     });
