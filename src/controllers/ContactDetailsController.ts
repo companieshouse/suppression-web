c47556c62ec75383b96f4c10eafb897ee75d97c7
akaJSaunders <me@jsaunders.dev>
2020-10-01 16:03:59 +0100

    add back button

--- src/controllers/ContactDetailsController.ts
+++ src/controllers/ContactDetailsController.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes/build';
 import { Address, SuppressionData } from '../models/SuppressionDataModel';
-import { CONTACT_DETAILS_PAGE_URI } from '../routes/paths';
+import { CONTACT_DETAILS_PAGE_URI, SERVICE_ADDRESS_PAGE_URI } from '../routes/paths';
 import SessionService from '../services/session/SessionService';
 import { ValidationResult } from '../utils/validation/ValidationResult';
 import { FormValidator } from '../validators/FormValidator';
 import { schema as formSchema } from '../validators/schema/AddressToRemoveSchema';
 
 const template = 'contact-details';
+const backNavigation = SERVICE_ADDRESS_PAGE_URI;
 
 export class ContactDetailsController {
 
@@ -23,6 +24,7 @@ export class ContactDetailsController {
 
     res.render(template, {
       ...suppressionData.contactAddress,
+      backNavigation
     });
   };
 
