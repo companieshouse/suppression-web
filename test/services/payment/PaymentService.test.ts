625417e20b67558e504596c27c32fd1ee0d4109d
Natalie Blanchette <16392604+n-r-b@users.noreply.github.com>
2020-11-25 12:00:11 +0000

    fix: add refresh token to payment api calls (#146)

--- test/services/payment/PaymentService.test.ts
+++ test/services/payment/PaymentService.test.ts
@@ -7,99 +7,177 @@ import { StatusCodes } from 'http-status-codes';
 import { PaymentStatus } from '../../../src/models/PaymentStatus';
 
 import { PaymentService } from '../../../src/services/payment/PaymentService';
+import { RefreshTokenService } from '../../../src/services/refresh-token/RefreshTokenService';
 
 const MockedAPIClient = ApiClient as jest.Mock<ApiClient>;
-const apiClientMock = new MockedAPIClient() as jest.Mocked<ApiClient>;
+const apiClientMockFailure = new MockedAPIClient() as jest.Mocked<ApiClient>;
+const apiClientMockSuccess = new MockedAPIClient() as jest.Mocked<ApiClient>;
 
 const mockApplicationReference: string = 'testID';
 const mockResourceUri: string = 'payments/testID'
 const mockPaymentStateUUID: string = '01234567';
 const mockToken: string = 'testToken';
+const mockNewToken: string = 'newTestToken';
+const mockRefreshToken: string = 'refresh-token';
+const mockRefreshClientId: string = '1';
+const mockRefreshClientSecret: string = 'ABC';
+const mockRefreshServiceUri: string = 'http://localhost/oauth2/token';
+const mockUrl = 'http://test.payments.gov.uk/123456';
+const mockResource = 'payments/TEST123456'
+
+const refreshTokenService: RefreshTokenService =
+  new RefreshTokenService(mockRefreshServiceUri, mockRefreshClientId, mockRefreshClientSecret);
+
+const mockResponseFailure = (statusCode) => failure({
+  httpStatusCode: statusCode,
+  errors: 'Test Error'
+});
 
 describe('PaymentService', () => {
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('generatePaymentUrl', () => {
 
+    const mockResponseSuccess = success({
+      resource: {
+        links: {
+          journey: mockUrl,
+          self: mockResource
+        }
+      } as Payment
+    } as ApiResponse<Payment>);
+
     it('should initiate GOV Pay payment and return a GOV Pay URL', async () => {
-      const mockUrl = 'http://test.payments.gov.uk/123456';
-      const mockResource = 'payments/TEST123456'
-      const mockResponse = success({
-        resource: {
-          links: {
-            journey: mockUrl,
-            self: mockResource
-          }
-        } as Payment
-      } as ApiResponse<Payment>);
       const payMock = jest.fn();
-      payMock.mockReturnValue(Promise.resolve(mockResponse));
-      apiClientMock.payment.createPayment = payMock;
+      payMock.mockReturnValue(Promise.resolve(mockResponseSuccess));
+      apiClientMockFailure.payment.createPayment = payMock;
 
-      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMock);
+      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMockFailure);
 
-      const paymentService = new PaymentService();
-      const result = await paymentService.generatePaymentUrl(mockApplicationReference, mockPaymentStateUUID, mockToken);
+      const paymentService = new PaymentService(refreshTokenService);
+      const result = await paymentService.generatePaymentUrl(mockApplicationReference, mockPaymentStateUUID, mockToken, mockRefreshToken);
       expect(payMock).toHaveBeenCalled();
       expect(result.redirectUrl).toEqual(mockUrl + '?summary=false');
       expect(result.resourceUri).toEqual(mockResource);
     });
 
     it('should throw an error when the CH Payment Wrapper fails', async () => {
-      const mockResponse = failure({
-        httpStatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        errors: 'Test Error'
-      });
       const payMock = jest.fn();
-      payMock.mockReturnValue(Promise.resolve(mockResponse));
-      apiClientMock.payment.createPayment = payMock;
+      payMock.mockReturnValue(Promise.resolve(mockResponseFailure(StatusCodes.INTERNAL_SERVER_ERROR)));
+      apiClientMockFailure.payment.createPayment = payMock;
 
-      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMock);
+      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMockFailure);
 
-      const paymentService = new PaymentService();
-      await expect(paymentService.generatePaymentUrl(mockApplicationReference, mockPaymentStateUUID, mockToken))
+      const paymentService = new PaymentService(refreshTokenService);
+      await expect(paymentService.generatePaymentUrl(mockApplicationReference, mockPaymentStateUUID, mockToken, mockRefreshToken))
         .rejects
         .toThrow('Failed to initiate payment - status: 500, error: Test Error');
+    });
+
+    it('should refresh token when the CH Payment Wrapper fails with 401 unauthorised', async () => {
+
+      apiClientMockFailure.payment.createPayment = jest.fn().mockReturnValue(Promise.resolve(mockResponseFailure(StatusCodes.UNAUTHORIZED)));
+      apiClientMockSuccess.payment.createPayment = jest.fn().mockReturnValue(Promise.resolve(mockResponseSuccess));
+
+      const apiClientSpy = jest.spyOn(nodeSdk, 'createApiClient')
+        .mockImplementationOnce(() => apiClientMockFailure)
+        .mockImplementationOnce(() => apiClientMockSuccess);
+
+      const refreshTokenSpy = jest.spyOn(RefreshTokenService.prototype, 'refresh').mockReturnValue(Promise.resolve(mockNewToken));
+
+      const paymentService = new PaymentService(refreshTokenService);
+      const result = await paymentService.generatePaymentUrl(mockApplicationReference, mockPaymentStateUUID, mockToken, mockRefreshToken);
+      expect(apiClientSpy).toHaveBeenCalledTimes(2);
+      expect(refreshTokenSpy).toHaveBeenCalledTimes(1);
+      expect(result.redirectUrl).toEqual(mockUrl + '?summary=false');
+      expect(result.resourceUri).toEqual(mockResource);
+    });
+
+    it('should throw an error when the CH Payment Wrapper fails with 401 unauthorised and refresh token fails', async () => {
+      apiClientMockFailure.payment.createPayment = jest.fn().mockReturnValue(Promise.resolve(mockResponseFailure(StatusCodes.UNAUTHORIZED)));
+
+      const apiClientSpy = jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMockFailure);
+
+      const refreshTokenSpy = jest.spyOn(RefreshTokenService.prototype, 'refresh').mockRejectedValue(new Error('Error!'));
+
+      const paymentService = new PaymentService(refreshTokenService);
+      await expect(paymentService.generatePaymentUrl(mockApplicationReference, mockPaymentStateUUID, mockToken, mockRefreshToken))
+        .rejects
+        .toThrow('Error!');
+      expect(apiClientSpy).toHaveBeenCalled();
+      expect(refreshTokenSpy).toHaveBeenCalled();
     });
 
   });
 
   describe('getPaymentStatus', () => {
 
+    const mockResponseSuccess = success({
+      resource: {
+        status: PaymentStatus.PAID
+      } as Payment
+    } as ApiResponse<Payment>);
+
     it('should retrieve the payment session’s status', async () => {
-      const mockResponse = success({
-        resource: {
-          status: PaymentStatus.PAID
-        } as Payment
-      } as ApiResponse<Payment>);
+      const mockResponse = mockResponseSuccess;
       const payMock = jest.fn();
       payMock.mockReturnValue(Promise.resolve(mockResponse));
-      apiClientMock.payment.getPayment = payMock;
+      apiClientMockFailure.payment.getPayment = payMock;
 
-      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMock);
+      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMockFailure);
 
-      const paymentService = new PaymentService();
-      const result = await paymentService.getPaymentStatus(mockResourceUri, mockToken);
+      const paymentService = new PaymentService(refreshTokenService);
+      const result = await paymentService.getPaymentStatus(mockResourceUri, mockToken, mockRefreshToken);
       expect(payMock).toHaveBeenCalled();
       expect(result).toEqual(PaymentStatus.PAID);
     });
 
     it('should throw an error when the CH Payment Wrapper fails', async () => {
-      const mockResponse = failure({
-        httpStatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        errors: 'Test Error'
-      });
       const payMock = jest.fn();
-      payMock.mockReturnValue(Promise.resolve(mockResponse));
-      apiClientMock.payment.getPayment = payMock;
+      payMock.mockReturnValue(Promise.resolve(mockResponseFailure(StatusCodes.INTERNAL_SERVER_ERROR)));
+      apiClientMockFailure.payment.getPayment = payMock;
 
-      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMock);
+      jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMockFailure);
 
-      const paymentService = new PaymentService();
-      await expect(paymentService.getPaymentStatus(mockResourceUri, mockToken))
+      const paymentService = new PaymentService(refreshTokenService);
+      await expect(paymentService.getPaymentStatus(mockResourceUri, mockToken, mockRefreshToken))
         .rejects
         .toThrow('Failed to verify payment status - status: 500, error: Test Error');
     });
 
-  });
+    it('should refresh token when the CH Payment Wrapper fails with 401 unauthorised', async () => {
+      apiClientMockFailure.payment.getPayment = jest.fn().mockReturnValue(Promise.resolve(mockResponseFailure(StatusCodes.UNAUTHORIZED)));
+      apiClientMockSuccess.payment.getPayment = jest.fn().mockReturnValue(Promise.resolve(mockResponseSuccess));
 
+      const apiClientSpy = jest.spyOn(nodeSdk, 'createApiClient')
+        .mockImplementationOnce(() => apiClientMockFailure)
+        .mockImplementationOnce(() => apiClientMockSuccess);
+
+      const refreshTokenSpy = jest.spyOn(RefreshTokenService.prototype, 'refresh').mockReturnValue(Promise.resolve(mockNewToken));
+
+      const paymentService = new PaymentService(refreshTokenService);
+      const result = await paymentService.getPaymentStatus(mockResourceUri, mockToken, mockRefreshToken);
+      expect(apiClientSpy).toHaveBeenCalledTimes(2);
+      expect(refreshTokenSpy).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(PaymentStatus.PAID);
+    });
+
+    it('should throw an error when the CH Payment Wrapper fails with 401 unauthorised and refresh token fails', async () => {
+      apiClientMockFailure.payment.getPayment = jest.fn().mockReturnValue(Promise.resolve(mockResponseFailure(StatusCodes.UNAUTHORIZED)));
+
+      const apiClientSpy = jest.spyOn(nodeSdk, 'createApiClient').mockImplementationOnce(() => apiClientMockFailure);
+
+      const refreshTokenSpy = jest.spyOn(RefreshTokenService.prototype, 'refresh').mockRejectedValue(new Error('Error!'));
+
+      const paymentService = new PaymentService(refreshTokenService);
+      await expect(paymentService.getPaymentStatus(mockResourceUri, mockToken, mockRefreshToken))
+        .rejects
+        .toThrow('Error!');
+      expect(apiClientSpy).toHaveBeenCalled();
+      expect(refreshTokenSpy).toHaveBeenCalled();
+    });
+  });
 });
